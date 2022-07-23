84d5c3221ce7662e6a4c8d1cb2b3a5d6b3cc904d
Sebastiaan Benjamins <45468731+cbasje@users.noreply.github.com>
2022-07-23 15:34:51 +0200

    Prettier

--- README.md
+++ README.md
@@ -2,10 +2,10 @@
 title: ExpressJS Prisma
 description: An ExpressJS server that uses Prisma to connect to a PostgreSQL database
 tags:
-  - express
-  - postgresql
-  - prisma
-  - typescript
+    - express
+    - postgresql
+    - prisma
+    - typescript
 ---
 
 # ExpressJS Prisma Example
@@ -16,25 +16,25 @@ This is an [ExpressJS](https://expressjs.com/) REST API that uses [Prisma](https
 
 ## ✨ Features
 
-- Prisma
-- Express
-- Postgres
-- TypeScript
+-   Prisma
+-   Express
+-   Postgres
+-   TypeScript
 
 ## 💁‍♀️ How to use
 
-- Install dependencies `yarn`
-- [Provision a Postgres container on Railway](https://dev.new)
-- Connect to your Railway project with `railway link`
-- Migrate the database `railway run yarn migrate:dev`
-- Run the Server app `railway run yarn dev`
+-   Install dependencies `yarn`
+-   [Provision a Postgres container on Railway](https://dev.new)
+-   Connect to your Railway project with `railway link`
+-   Migrate the database `railway run yarn migrate:dev`
+-   Run the Server app `railway run yarn dev`
 
 ## 📝 Notes
 
 This is a simple REST API for todo items. The available routes are
 
-- `GET /todos` gets all todos
-- `POST /todos` creates a new using `text` in the JSON body
-- `GET /todos/:id` gets a todo by id
-- `PUT /todos/:id` updates a todo by id
-- `DELETE /todos/:id` deletes a todo by id
+-   `GET /todos` gets all todos
+-   `POST /todos` creates a new using `text` in the JSON body
+-   `GET /todos/:id` gets a todo by id
+-   `PUT /todos/:id` updates a todo by id
+-   `DELETE /todos/:id` deletes a todo by id
--- src/index.ts
+++ src/index.ts
@@ -11,66 +11,66 @@ app.use(express.raw({ type: "application/vnd.custom-type" }));
 app.use(express.text({ type: "text/html" }));
 
 app.get("/todos", async (req, res) => {
-  const todos = await prisma.todo.findMany({
-    orderBy: { createdAt: "desc" },
-  });
+    const todos = await prisma.todo.findMany({
+        orderBy: { createdAt: "desc" },
+    });
 
-  res.json(todos);
+    res.json(todos);
 });
 
 app.post("/todos", async (req, res) => {
-  const todo = await prisma.todo.create({
-    data: {
-      completed: false,
-      createdAt: new Date(),
-      text: req.body.text ?? "Empty todo",
-    },
-  });
+    const todo = await prisma.todo.create({
+        data: {
+            completed: false,
+            createdAt: new Date(),
+            text: req.body.text ?? "Empty todo",
+        },
+    });
 
-  return res.json(todo);
+    return res.json(todo);
 });
 
 app.get("/todos/:id", async (req, res) => {
-  const id = req.params.id;
-  const todo = await prisma.todo.findUnique({
-    where: { id },
-  });
+    const id = req.params.id;
+    const todo = await prisma.todo.findUnique({
+        where: { id },
+    });
 
-  return res.json(todo);
+    return res.json(todo);
 });
 
 app.put("/todos/:id", async (req, res) => {
-  const id = req.params.id;
-  const todo = await prisma.todo.update({
-    where: { id },
-    data: req.body,
-  });
+    const id = req.params.id;
+    const todo = await prisma.todo.update({
+        where: { id },
+        data: req.body,
+    });
 
-  return res.json(todo);
+    return res.json(todo);
 });
 
 app.delete("/todos/:id", async (req, res) => {
-  const id = req.params.id;
-  await prisma.todo.delete({
-    where: { id },
-  });
+    const id = req.params.id;
+    await prisma.todo.delete({
+        where: { id },
+    });
 
-  return res.send({ status: "ok" });
+    return res.send({ status: "ok" });
 });
 
 app.get("/", async (req, res) => {
-  res.send(
-    `
+    res.send(
+        `
   <h1>Todo REST API</h1>
   <h2>Available Routes</h2>
   <pre>
     GET, POST /todos
     GET, PUT, DELETE /todos/:id
   </pre>
-  `.trim(),
-  );
+  `.trim()
+    );
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+    console.log(`Example app listening at http://localhost:${port}`);
 });
